440b33aa958ab43e12478f0e5d7deb198f50df89
arnabkdutta <duttaarnab71@gmail.com>
2022-08-07 19:15:46 +0530

    file upload function

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import express from 'express';
 import api from './route/route'
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import cluster from 'cluster';
-import http from 'http';
-import { cpus } from 'os';
-import process from 'process';
-
-const numCPUs = cpus().length;
-console.log(numCPUs);
+import multer from 'multer';
+import * as service from './service/service';
+// const service = require('./service/service');
 
 const app = express();
+
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 //app.use(bodyParser.json({ type: 'application/*+json' }));
-app.use(bodyParser.text({ type: 'text/html' }));
+//app.use(bodyParser.text({ type: 'text/html' }));
 
-const route = () => api({}, app);
+
+const route = () => api(service , app);
 route();
-
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log(`Server has started at ${port}`));
--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,18 @@
-module.exports = ({ }, app) => {
+module.exports = (service, app) => {
     app.get('/', (req, res) => {
         res.send('Basic route is working');
     });
 
     app.post('/post_from_data', (req, res) => {
-        console.log(req.body);
+        console.log(req.file);
         res.send(req.body);
     });
+
+    app.post('/upload', service.fileUpload().single('fileName'), (req, res, next) => {
+        // res.send('Uploaded')
+        console.log(req.file);
+        res.send({
+            msg : 'upload successfully'
+        })
+    })
 }
